4ba5ffc5239da4fb564c1a01b36549948cdc3a8e
Daniel Bessonov <dbessonov@itrus-macbook-pro.local>
2017-06-27 15:08:09 -0700

    Initial Commit

---
+++ CryptoApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CryptoApp
+//
+//  Created by Daniel Bessonov on 6/27/17.
+//  Copyright © 2017 Daniel Bessonov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
